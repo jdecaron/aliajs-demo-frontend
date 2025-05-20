b9bae88776f277e38feffbeca8e7d54086afbd4a
Jean Denis Caron <j@rotat.io>
2025-05-20 12:31:38 -0400

    Removing extra docs.

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,21 +6,8 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
+			title: 'AliaJS Demo',
+			
 		}),
 	],
 });
